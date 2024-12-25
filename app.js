9e3726c1cf6d063921c645609630f3b48d445b74
somalingck <somalingck@gmail.com>
2024-12-25 11:34:06 +0530

    first commit

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
 import { dbConnection } from "./database/dbConnection.js";
-// import { errorMiddleware } from "./error/error.js";
 import { errrorMiddleware } from "./error/error.js";
 import reservationRouter from "./routes/reservation.js";
 
@@ -47,8 +46,8 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["POST"],
+    origin: process.env.FRONTEND_URL, // Ensure this matches the frontend's URL
+    methods: ["GET", "POST", "OPTIONS"], // Allow OPTIONS for preflight requests
     credentials: true,
   })
 );
